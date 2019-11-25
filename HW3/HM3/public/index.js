1e052e0fe1e8d6f5bb8ec6d13edeff0acde732a0
Dzmitry Fitsner <dzmitry.fitsner@leverx.com>
2019-11-25 10:08:28 +0300

    Add HW3

---
+++ HW3/HM3/public/index.js
@@ -0,0 +1,256 @@
+function Book(title,  fieldOfStudy, publisher, author) {
+this.title = title;
+this.fieldOfStudy = fieldOfStudy;
+this.publisher = publisher;
+this.author = author;
+
+this.setTitle = function (value) {
+  title = value;
+}
+this.getTitle = function () {
+  return this.title;
+}
+
+this.setFieldOfStudy = function (value) {
+  fieldOfStudy = value;
+}
+this.getFieldOfStudy = function () {
+  return this.fieldOfStudy;
+}
+
+this.setPublisher = function (value) {
+  publisher = value;
+} 
+this.getPublisher = function () {
+  return this.publisher;
+}
+
+this.setAuthor = function (value) {
+  author = value;
+}
+this.getAuthor = function () {
+  return this.author;
+}
+
+}
+
+function Audiobook(title,  fieldOfStudy, publisher, author, length) {
+  Book.call(this, title,  fieldOfStudy, publisher, author);
+  this.length = length;
+  this.setLength = function (value) {
+    length = value;
+  }
+  this.getLength = function () {
+    return length;
+  }
+} 
+
+function Textbook(title,  fieldOfStudy, publisher, author, numberOfPages) {
+  Book.call(this, title,  fieldOfStudy, publisher, author);
+  this.setNumberOfPages = function (value) {
+    numberOfPages = value;
+  }
+  this.getNumberOfPages = function () {
+    return numberOfPages;
+  }
+}
+
+function onCreate(ev) {
+  ev.preventDefault();
+ 
+ 
+  var data = JSON.stringify({
+      "Title" : String(document.getElementById("ctitle").value),
+      "FieldOfStady": String(document.getElementById("cfieldOfStudy").value),
+      "Publisher": String(document.getElementById("cpublisher").value),
+      "Author": String(document.getElementById("cauthor").value)
+  });
+  console.log(data);
+
+  xhr = new XMLHttpRequest();
+  xhr.withCredentials = true;
+  xhr.addEventListener("readystatechange", function () {
+      if (this.readyState === 4) {
+          alert(this.responseText);
+          document.getElementById("createForm").dispatchEvent(new Event('submit'));
+      } 
+  });
+
+  xhr.open("POST", "http://localhost:2403/hw3");
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.send(data);
+}
+
+function onRead() {
+    console.log('allah');
+    xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.addEventListener("readystatechange", function () {
+
+      if (this.readyState === 4) {
+
+          result=JSON.parse(this.response);
+          var resultTBody=document.createElement('tbody');
+
+          result.map(function(nthCPU){
+              resultTBody.appendChild(parseCPUToTableRow(nthCPU));
+          });
+
+          var table=document.getElementById('rTBody').parentElement;
+          table.replaceChild(resultTBody,document.getElementById('rTBody'));
+          resultTBody.id='rTBody';
+      }
+
+  });
+
+    xhr.open("GET", "http://localhost:2403/hw3");
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send();
+}
+
+function parseCPUToTableRow(CPUs){
+  var row=document.createElement('tr');
+
+  id=document.createElement('th');
+  id.innerText=CPUs['id'];
+  row.appendChild(id);
+
+  title=document.createElement('td');
+  title.innerText=CPUs['Title'];
+  row.appendChild(title);
+
+  fieldOfStudy=document.createElement('td');
+  fieldOfStudy.innerText=CPUs['FieldOfStady'];
+  row.appendChild(fieldOfStudy);
+ 
+  publisher=document.createElement('td');
+  publisher.innerText=CPUs['Publisher'];
+  row.appendChild(publisher);
+  
+  author=document.createElement('td');
+  author.innerText=CPUs['Author'];
+  row.appendChild(author);
+
+  return row;
+}
+
+function onPrepareUpdate(ev){
+  ev.preventDefault();
+  xhrids = new XMLHttpRequest();
+  xhrids.withCredentials = true;
+
+  xhrids.addEventListener("readystatechange", function () {
+      if (this.readyState === 4) {
+          //console.log(this.response);
+          result=JSON.parse(this.response);
+          var ids=document.createElement('select');
+          ids.className='form-control';
+          result.map(function(nthCPU){
+              var id=document.createElement('option');
+              id.innerHTML=nthCPU['id'];
+              ids.appendChild(id);
+          });
+          var form=document.getElementById('uid').parentElement;
+          form.replaceChild(ids,document.getElementById('uid'));
+          ids.id='uid';
+      }
+  });
+  xhrids.open("GET", "http://localhost:2403/hw3");
+  xhrids.setRequestHeader("Content-Type", "application/json");
+  xhrids.send();
+}
+
+
+function onUpdate(ev) {
+  ev.preventDefault();
+
+ 
+  var data = JSON.stringify({
+    "Title" : String(document.getElementById("utitle").value),
+    "FieldOfStady": String(document.getElementById("ufieldOfStady").value),
+    "Publisher": String(document.getElementById("upublisher").value),
+    "Author": String(document.getElementById("uauthor").value)
+  });
+  console.log(data);
+  xhr = new XMLHttpRequest();
+  xhr.withCredentials = true;
+
+  xhr.addEventListener("readystatechange", function () {
+      if (this.readyState === 4) {
+          console.log(this.responseText);
+      }
+  });
+
+  xhr.open("PUT", "http://localhost:2403/hw3/"+document.getElementById("uid").value);
+  xhr.setRequestHeader("Content-Type", "application/json");
+  console.log(document.getElementById("uid").value);
+  xhr.send(data);
+}
+
+
+function onPrepareDelete(ev){
+  ev.preventDefault();
+  xhrids = new XMLHttpRequest();
+  xhrids.withCredentials = true;
+
+  xhrids.addEventListener("readystatechange", function () {
+      if (this.readyState === 4) {
+          //console.log(this.response);
+          result=JSON.parse(this.response);
+          var ids=document.createElement('select');
+          ids.className='form-control';
+          result.map(function(nthCPU){
+              var id=document.createElement('option');
+              id.innerHTML=nthCPU['id'];
+              ids.appendChild(id);
+          });
+          var form=document.getElementById('did').parentElement;
+          form.replaceChild(ids,document.getElementById('did'));
+          ids.id='did';
+      }
+  });
+  xhrids.open("GET", "http://localhost:2403/hw3");
+  xhrids.setRequestHeader("Content-Type", "application/json");
+  xhrids.send();
+}
+
+
+function onDelete(ev) {
+  ev.preventDefault();
+   xhr = new XMLHttpRequest();
+  xhr.withCredentials = true;
+
+  xhr.addEventListener("readystatechange", function () {
+      if (this.readyState === 4) {
+          console.log(this.responseText);
+      }
+  });
+
+  xhr.open("DELETE", "http://localhost:2403/hw3/"+document.getElementById("did").value);
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.send();
+}
+
+(function () {
+  
+  document.getElementById('cbutton').addEventListener(
+      'click',  onCreate 
+  );
+  document.getElementById('rbutton').addEventListener(
+      'click', onRead
+  );
+  document.getElementById('ubutton').addEventListener(
+      'click', onUpdate
+  );
+  document.getElementById('pubutton').addEventListener(
+      'click', onPrepareUpdate
+  );
+  document.getElementById('dbutton').addEventListener(
+      'click', onDelete
+  );
+  document.getElementById('pdbutton').addEventListener(
+    'click', onPrepareDelete
+);
+  console.log('Handlers is set');
+})()
