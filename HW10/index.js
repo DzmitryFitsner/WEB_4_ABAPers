3e4003ad9091296246bedb4b754c7113c5b3bda8
Dzmitry Fitsner <dzmitry.fitsner@leverx.com>
2020-01-06 12:17:46 +0300

    Add HW10

--- HW10/index.js
+++ HW10/index.js
@@ -21,6 +21,7 @@ class User{
 class Worker extends User{
   constructor(name, lastName, dataOfBirth, place){
     super(name, lastName, dataOfBirth)
+    
       let _place = place;
 
     this.getPlace = function(){
